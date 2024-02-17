84862b2992d36838c75959ee568288f6a148aa76
unknown <abdelhamed.muhammed19@gmail.com>
2024-02-18 00:14:22 +0200

    added

--- MyEcommerce/app/routes.php
+++
@@ -1,6 +0,0 @@
-<?php
-$rout->get("","pagecontroller@index");
-$rout->get("payment","pagecontroller@payment");
-$rout->post("/payment","pagecontroller@index");
-
-
--- MyEcommerce/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-require "vendor/autoload.php";
-use core\router;
-use core\request;
-require "core/bootstrap.php";
-router::load("app/routes.php")->direct(request::uri(),request::method());
-
-//require "app/views/index.php";
-//require "app/views/payment.php";
